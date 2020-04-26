6ebd4bd1e565f7e678e73f12d80eb1dd707b6a18
Superknowa Courses <59803143+superknowa@users.noreply.github.com>
2020-04-26 10:21:36 +0300

    Update Başlangıç Kodları.dart

--- Başlangıç Kodları.dart
+++ Başlangıç Kodları.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'İlk Projem',
       theme: ThemeData(
         primarySwatch: Colors.green,
